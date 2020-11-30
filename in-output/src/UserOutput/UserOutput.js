9b7e0383e27b6141692549760341167df521809a
troutbum <64603749+andy.pedonti@users.noreply.github.com>
2020-11-29 19:19:06 -0500

    input and output components

---
+++ in-output/src/UserOutput/UserOutput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const userOutput = () => {
+    return (
+        <div>
+            <p>Some Random Text</p>
+            <p>I Hope I Get Overwritten</p>
+        </div>
+    );
+};
+
+export default userOutput;
